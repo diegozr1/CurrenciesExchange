5fcef2366bbbfab3675a3c5f2352397bc49a2445
Diego Zamora <diego.zr@live.com.mx>
2018-02-05 14:31:29 -0600

    New design and more currencies supported

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Extensión de Google Chrome perfecta para comercio electrónico y viajes.
 Si vives en México ahora puedes saber cuanto dinero necesitas para tus compras en Estados Unidos en tiempo real.
 
 Incluye tasa de cambio de bitcoin para compras por internet mas completas.
+
+- https://developers.coinbase.com/api/v2#exchange-rates
+- http://fixer.io/
+- https://bitso.com/developers#available-books
--- js/app.js
+++ js/app.js
@@ -1,9 +1,38 @@
 var app = angular.module("App", []);
-app.controller("Money", function($scope, $http) {    
-    $http.get("https://api.coinbase.com/v1/currencies/exchange_rates")
+app.controller("Money", function($scope, $http) {           
+    $http.get("https://api.fixer.io/latest?symbols=USD,EUR,CAD,JPY&base=MXN")
+      .success(function(data){
+        $scope.CAD= parseFloat(data.rates.CAD)
+        $scope.EUR= parseFloat(data.rates.EUR)
+        $scope.USD= parseFloat(data.rates.USD)
+        $scope.JPY= parseFloat(data.rates.JPY)
+      });
+    $http.get("https://api.fixer.io/latest?symbols=MXN&base=USD")
+      .success(function(data){        
+        $scope.USDBASE= parseFloat(data.rates.MXN)
+      });
+      $http.get("https://api.fixer.io/latest?symbols=MXN&base=CAD")
+      .success(function(data){        
+        $scope.CADBASE= parseFloat(data.rates.MXN)
+      });
+      $http.get("https://api.fixer.io/latest?symbols=MXN&base=EUR")
+      .success(function(data){        
+        $scope.EURBASE= parseFloat(data.rates.MXN)
+      });
+    $http.get("https://api.bitso.com/v3/ticker/?book=btc_mxn")
       .success(function(data) {
-        $scope.money = data;
-        $scope.dolar = parseFloat(data.mxn_to_usd);
-        $scope.bitcoin = parseFloat(data.mxn_to_btc);
-      });    
+        $scope.BTC= parseFloat(data.payload.last)
+      });
+    $http.get("https://api.bitso.com/v3/ticker/?book=xrp_mxn")
+      .success(function(data) {
+        $scope.XRP= parseFloat(data.payload.last)
+      });  
+    $http.get("https://api.bitso.com/v3/ticker/?book=eth_mxn")
+      .success(function(data){
+        $scope.ETH= parseFloat(data.payload.last)
+      });
+    $http.get("https://api.bitso.com/v3/ticker/?book=ltc_mxn")
+      .success(function(data){
+        $scope.LTC= parseFloat(data.payload.last)
+      }); 
 });
